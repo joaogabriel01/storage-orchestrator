ba2e37374c14331a66df21358d000f418489a0f7
João Azevedo <joaogfazevedo@gmail.com>
2024-08-17 19:04:37 -0300

    fix: add mutex in orchestrator and returns a copy of the unit map for internal protection

--- pkg/orchestrator.go
+++ pkg/orchestrator.go
@@ -2,25 +2,38 @@ package pkg
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/joaogabriel01/storage-orchestrator/pkg/protocols"
 )
 
 type Orchestrator[K any, V any] struct {
+	mu               sync.RWMutex
 	units            map[string]protocols.StorageUnit[K, V]
 	typeOrchestrator uint
 }
 
 func (o *Orchestrator[K, V]) AddUnit(storageName string, storage protocols.StorageUnit[K, V]) error {
+	o.mu.Lock()
+	defer o.mu.Unlock()
 	o.units[storageName] = storage
 	return nil
 }
 
 func (o *Orchestrator[K, V]) GetUnits() (map[string]protocols.StorageUnit[K, V], error) {
-	return o.units, nil
+	o.mu.RLock()
+	defer o.mu.RUnlock()
+
+	unitsCopy := make(map[string]protocols.StorageUnit[K, V])
+	for k, v := range o.units {
+		unitsCopy[k] = v
+	}
+	return unitsCopy, nil
 }
 
 func (o *Orchestrator[K, V]) GetUnit(unitName string) (protocols.StorageUnit[K, V], error) {
+	o.mu.RLock()
+	defer o.mu.RUnlock()
 	unit, exists := o.units[unitName]
 	if !exists {
 		return nil, fmt.Errorf("unit not found")
