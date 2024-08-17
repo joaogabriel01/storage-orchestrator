eddbb758e135c59885b9ef479bd1d44c1d468aa7
João Azevedo <joaogfazevedo@gmail.com>
2024-08-17 18:09:26 -0300

    feat: improve storageOrchestrator interface

--- pkg/protocols/storage_orchestrator.go
+++ pkg/protocols/storage_orchestrator.go
@@ -2,16 +2,27 @@ package protocols
 
 import "context"
 
-type Options interface {
-	GetTypeOrchestrator() uint
-	GetContext() context.Context
-}
+type typeOrchestrator uint
+
+const (
+	Cache typeOrchestrator = iota
+	Race
+)
 
 type StorageOrchestrator[K any, V any] interface {
 	Save(item V, opt ...Options) error
 	Get(query K, opt ...Options) (V, error)
 	Delete(query K, opt ...Options) error
 	Sync(from string, to []string, opt ...Options) error
+
+	AddUnit(storage StorageUnit[K, V], storageName string) error
+	GetUnits() (map[string]StorageUnit[K, V], error)
+	GetUnit(string) (StorageUnit[K, V], error)
+}
+
+type Options interface {
+	GetTypeOrchestrator() uint
+	GetContext() context.Context
 }
 
 type StorageUnit[K any, V any] interface {
